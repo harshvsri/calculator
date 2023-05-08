6444380e68da744445db9f0809199280d14743de
Harsh V Srivastava <harshvsri@gmail.com>
2023-05-08 23:40:06 +0530

    Complete Basic Logic Design - no multi-operands support

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 //* DISPLAY
-let clear = false;
-let firstNum = 12;
-let secondNum = 23;
-let operator = "+";
-
-if (clear) {
-  res = 0;
-}
-
-let res = 0;
 const display = document.querySelector(".display");
 const calculationResult = document.createElement("p");
 calculationResult.classList.add("calculation-result");
-calculationResult.innerText = res;
+updateDisplay("0");
 display.appendChild(calculationResult);
+
+function updateDisplay(value) {
+  calculationResult.innerText = value;
+}
 
 //* BUTTONS
 const buttonContainer = document.querySelector(".calculator .buttons");
 const tokens = [
   "AC",
   "del",
+  "+/-",
   "^",
-  "%",
   "7",
   "8",
   "9",
@@ -39,45 +33,71 @@ const tokens = [
   "=",
   "*",
 ];
+
+// Rendering Buttons
 for (let i = 0; i < tokens.length; i++) {
   const button = document.createElement("div");
   button.classList.add("button");
   button.innerText = tokens[i];
   buttonContainer.appendChild(button);
 }
+
+// Adding EventListeners
 const buttons = document.querySelectorAll(".button");
+let inputEquation = "";
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
-    const pressed = event.target.innerText;
-    if (pressed == "AC") {
-      clear = true;
+    const buttonPressed = event.target.innerText;
+    if (buttonPressed == "=") {
+      inputEquation = getResult(inputEquation);
+      updateDisplay(inputEquation);
+    } else if (buttonPressed == "AC") {
+      inputEquation = "";
+      updateDisplay("0");
+    } else if (buttonPressed == "del") {
+      if (inputEquation.length == 1) {
+        inputEquation = "";
+        updateDisplay("0");
+      } else {
+        inputEquation = inputEquation.slice(0, inputEquation.length - 1);
+        updateDisplay(inputEquation);
+      }
+    } else {
+      inputEquation += buttonPressed;
+      updateDisplay(inputEquation);
     }
-    if (pressed == "=") {
-      val = operate(firstNum, secondNum, operator);
-    }
-    eqn += pressed;
-    console.log(eqn);
   });
 });
 
 //* OPERATE FUNCTION
 const operate = (num1, num2, operator) => {
-  num1 = +num1;
-  num2 = +num2;
   switch (operator) {
     case "+":
       return num1 + num2;
     case "-":
-      return num1 + num2;
+      return num1 - num2;
     case "*":
-      return num1 + num2;
+      return num1 * num2;
     case "/":
-      return num1 + num2;
+      return (num1 / num2).toPrecision(3);
     case "^":
-      return num1 ** num2;
-    case "+":
-      return num1 + num2;
-    default:
-      return "Maths Error";
+      return (num1 ** num2).toPrecision(3);
+  }
+};
+
+//* GET RESULT
+const operators = ["+", "-", "*", "/", "^"];
+const getResult = (string) => {
+  let i = 0;
+  while (i < string.length) {
+    if (operators.includes(string.charAt(i))) {
+      // Assigning values
+      const firstNum = Number(string.slice(0, i));
+      const operator = string.charAt(i);
+      const secondNum = Number(string.slice(i + 1, string.length));
+
+      return operate(firstNum, secondNum, operator);
+    }
+    i++;
   }
 };
